7acb87b88f18d340064efec42f37ee230c93e835
CRISTIAN MORA <mora.cristian.08@itisdalmine.edu.it>
2026-02-28 09:46:53 +0100

    gioco iniziale completato: verifica di vittoria, Inserimento parola...

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ async function scaricoDati(file) {
         .then(response => response.json())
         .then(data => {
             listaSoluzioni = data;
-            datiCaricati = true;
         })
 }
 function randomInt(min, max) {
@@ -13,14 +12,56 @@ function randomInt(min, max) {
 }
 
 //FUNZIONE PAROLA SEGRETA:
-function creaParolaSegreta(lista){
-    nRandom = randomInt(0, lista.lenght);
+function creaParolaSegreta(lista) {
+    nRandom = randomInt(0, lista.length - 1);
     parolaSegreta = lista[nRandom];
 }
 
-//FUNZIONE INIZIALE: scarico tutto il json su una variabile e poi INIZIO CON IL RESTO DEL PROGRAMMA
+//FUNZIONE CONTROLLO PAROLA:
+function controlloParola(parolaInput, parolaDaindovinare) {
+    if (parolaInput == parolaDaindovinare) {
+        return true;
+    }
+    return false;
+}
 
+
+//FUNZIONE INIZIALE: scarico tutto il json su una variabile e poi INIZIO CON IL RESTO DEL PROGRAMMA
 scaricoDati("/5lettere.json").then(() => {
     creaParolaSegreta(listaSoluzioni)
     console.log(parolaSegreta)
+
+    //QUANDO SCRIVO PASSO ALLA RIGA SUCCESSIVA
+    document.querySelectorAll('.cella').forEach((input, index, array) => {
+        input.addEventListener('input', () => {
+            if (input.value.length === 1 && index < array.length - 1) {
+                array[index + 1].focus(); // passa alla cella successiva
+            }
+        });
+    });
+    //PAROLA INSERITA:
+    let parola;
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            parola = ''; // inizializza come stringa vuota!
+
+            document.querySelectorAll(".cella").forEach((cella) => {
+                parola += cella.value; // prendi il valore dell'input
+            });
+
+            console.log(parola);
+        }
+        if (controlloParola(parola, parolaSegreta)) {
+            alert("Hai vinto! premi ok per fare una nuova partita!!")
+            setTimeout(() => {
+                location.reload();
+            }, 1000); // refresh dopo 1 secondi
+        }
+        else {
+            //to do
+        }
+    });
+
+
+
 });
